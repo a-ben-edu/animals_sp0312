bca03d34196787da04dc1c48095269d88fee8d3a
Elliot <09ellgra@skola.boras.se>
2026-03-12 13:38:02 +0100

    Hund

--- animals.py
+++ animals.py
@@ -2,12 +2,15 @@ def main():
     while True:
         ## Skriv ut menyvalen
         print("1. Cat")
+        print("2. Dog")
         print("Enter x to exit")
 
         choice = input("Enter your selection")
         match choice.lower():
             case '1': #Cat-sound
                 print("Mjau, mjau")
+            case '2': #Dog-sound
+                print("Voff, voff")
             case 'x': #Exit
                 break
             case _:

dcc20bc2f528617bc4f8d3c1f4b6fb1b33a746d0
Harry <09hareks@skola.boras.se>
2026-03-12 13:53:36 +0100

    Duck

--- animals.py
+++ animals.py
@@ -3,6 +3,7 @@ def main():
         ## Skriv ut menyvalen
         print("1. Cat")
         print("2. Dog")
+        print("3. Duck")
         print("Enter x to exit")
 
         choice = input("Enter your selection")
@@ -11,6 +12,8 @@ def main():
                 print("Meow, meow")
             case '2': #Dog-sound
                 print("Woof, woof")
+            case '3': #Duck-sound
+                print("Quack, quack")
             case 'x': #Exit
                 break
             case _:
